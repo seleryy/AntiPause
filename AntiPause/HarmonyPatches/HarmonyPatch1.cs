3de9bf19060e484b5aee6ce3ee13fbd71d1ff33b
alana <alana@DESKTOP-4KAUDUG>
2022-08-13 16:30:24 -0400

    Add project files.

---
+++ AntiPause/HarmonyPatches/HarmonyPatch1.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// See https://github.com/pardeike/Harmony/wiki for a full reference on Harmony.
+/// </summary>
+namespace AntiPause.HarmonyPatches
+{
+    [HarmonyPatch(typeof(GamePause), nameof(GamePause.Pause))]
+    internal class PatchPauseMenu
+    {
+
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
